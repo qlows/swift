b2a25c58038ce7ee977f86f7da8560ae5877c94d
qlows <79415956+qlows@users.noreply.github.com>
2023-04-11 13:55:53 -0400

    ex4

---
+++ ex4/GreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GreenViewController.swift
+//  ex4
+//
+//  Created by Umit Kilinc on 2023-04-11.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Green Screen";
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ex4/ex4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ex4
+//
+//  Created by Umit Kilinc on 2023-04-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
